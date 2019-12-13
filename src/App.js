db33828139c5d19ed89dba4b45f092b3e24e34fb
SirRichardM <richardmoeb@gmail.com>
2019-12-13 17:01:44 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  componentDidMount = async () => {
+    let result = await axios.get("https://api.rawg.io/api/games?search=doom")
+    console.log(result)
+    console.log(result.data.results)
+
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <img src="https://ih0.redbubble.net/image.850655622.6681/flat,750x1000,075,f.u1.jpg" alt=""  />
+          <div className="text-block">
+            <h1>Horror Times</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
